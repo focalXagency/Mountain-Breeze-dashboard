be6fcc48d481d3305407d44445c82dd0b8d1d9e6
Batoul Hassan <batoulhassan264@gmail.com>
2023-09-07 12:43:53 +0300

    files added

---
+++ assets/javascript/index.js
@@ -0,0 +1,11 @@
+
+
+// fetch("https://mountain.lavetro-agency.com/api/dashboard/articles/")
+// .then(res => res.json())
+// .then(res => console.log(res))
+// .catch(error => console.log(error));
+
+// fetch("https://mountain.lavetro-agency.com/api/dashboard/foods")
+// .then(res => res.json())
+// .then(res => console.log(res))
+// .catch(error => console.log(error));
---
+++ assets/javascript/newArticle.js
@@ -0,0 +1,50 @@
+const addImage = document.querySelector('#input-img');
+const title = document.querySelector('#input-title');
+//const selectedDate = document.querySelector('#input-date');
+const tag = document.querySelector('#tags');
+const desc = document.querySelector('#desc');
+const photo = document.querySelector('#input-photo');
+const addArticle = document.querySelector('.add-a');
+const newArticleForm = document.querySelector('.new-article-form');
+
+let json_addImage;
+let json_title;
+let dateArticle;
+let  json_desc;
+
+addImage.addEventListener("input", () => {
+   if (addImage.files.length) {
+    let json_addImage = addImage.files[0].name;
+    // console.log(addImage.files[0])
+   } else {
+    console.log('Please add an image')
+   }
+})
+
+title.addEventListener('input', () => {
+    json_title = title.value;
+    // console.log(title.value)
+})
+
+
+
+let date = new Date();
+document.querySelector('#input-date').value = date;
+inputDate.addEventListener('input', () => {
+     dateArticle = new Date(inputDate.value);
+    console.log(dateArticle);
+})
+
+
+photo.addEventListener("input", () => {
+    if (photo.files.length) {
+     let json_photos = photo.files[0].name;
+     console.log(photo.files[0])
+    } else {
+     console.log('Please add an image')
+    }
+ })
+
+
+//console.log('descData', descData)
+
